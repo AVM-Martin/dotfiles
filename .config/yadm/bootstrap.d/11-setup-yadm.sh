44d0452e252e4b09730e08be633f50f030b579f4
AVM.Martin <avmm.martin+github@gmail.com>
2026-04-24 13:50:57 +0700

    feat(config,yadm): move gitconfig into dedicated files
    Refs: a99e5d62fd80a590fd92db4668085466521eb8aa

--- .config/yadm/bootstrap.d/11-setup-yadm.sh
+++ .config/yadm/bootstrap.d/11-setup-yadm.sh
@@ -12,9 +12,6 @@ fi
 # clone submodules
 "${YADM_BIN_PATH}" submodule update --init
 
-# configure
-"${YADM_BIN_PATH}" gitconfig alias.ls-ignored "ls-files -o -i -x 'local.*'"
-
 # symlink
 mkdir -p "${HOME}/bin"
 ln -sf "${YADM_BIN_PATH}" "${HOME}/bin/yadm"
